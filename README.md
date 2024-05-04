ba3cce06eb01ac3fddef276c15a988cccb8c04ab
Hunter Koppen <hunter.koppen@kobeon.nl>
2024-05-04 14:00:38 +0200

    Bugfix ios limitations

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Android and iOS devices.
 To enable the Keep Awake functionality on your web pages, simply add the widget into the pages where you want to prevent
 the display from sleeping. The widget will take care of the rest.
 
+## Limitations
+
+Due to certain browsers limitations, atleast one click interaction is required before the widget is activated. A way to make sure the widget is always activated you could for example add it to a layout.
+
 ## Issues, suggestions and feature requests
 
 [\[link to GitHub issues\]](https://github.com/hunter-koppen/keep-awake-web/issues)
--- src/components/KeepAwake.jsx
+++ src/components/KeepAwake.jsx
@@ -1,34 +1,28 @@
-import { Component, createRef, createElement } from "react";
+import { Component, createElement } from "react";
 
-import NoSleep from "nosleep.js";
+import NoSleep from "@zakj/no-sleep";
 
 export class KeepAwake extends Component {
     constructor(props) {
         super(props);
         this.noSleep = new NoSleep();
         this.wakeLockEnabled = false;
-        this.buttonRef = createRef();
     }
 
     componentDidMount() {
-        this.buttonRef.current.click();
+        document.addEventListener("click", this.enableNoSleep, false);
     }
 
     componentWillUnmount() {
         this.noSleep.disable();
     }
 
-    handleClick = () => {
-        if (!this.wakeLockEnabled) {
-            this.noSleep.enable();
-            this.wakeLockEnabled = true;
-        } else {
-            this.noSleep.disable();
-            this.wakeLockEnabled = false;
-        }
+    enableNoSleep = () => {
+        document.removeEventListener("click", this.enableNoSleep, false);
+        this.noSleep.enable();
     };
 
     render() {
-        return <btn onClick={this.handleClick} ref={this.buttonRef} className="hidden"></btn>;
+        return null;
     }
 }
